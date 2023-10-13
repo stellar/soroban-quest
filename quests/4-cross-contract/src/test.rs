fd94ee359fdeb00a591efbae300d27b75a82226f
Elliot Voris <elliot@stellar.org>
2023-10-13 13:57:32 -0500

    quest 4 updates

--- quests/4-cross-contract/src/test.rs
+++ quests/4-cross-contract/src/test.rs
@@ -10,9 +10,6 @@ fn get_cross_call() {
     // Create a default Soroban environment.
     let env = Env::default();
 
-    // Disable checks for authentication. See note in quest 2 tests for details.
-    env.mock_all_auths();
-
     // Here we register the DataStore contract in the Soroban environment, and
     // build a client that can be used to invoke the contract.
     let storage_contract_address = env.register_contract_wasm(None, storage_contract::WASM);
@@ -24,15 +21,18 @@ fn get_cross_call() {
     let cross_call_contract_client =
         CrossContractCallContractClient::new(&env, &cross_call_contract_address);
 
-    // We generate a test user, and invoke the `put` function to store some data
-    // in the `DataStore` contract.
-    let u1 = Address::random(&env);
-    storage_contract_client.put(&u1, &bytes![&env, 0x48656c6c6f20536f726f62616e21]);
+    // Disable checks for authentication. See note in quest 2 tests for details.
+    env.mock_all_auths();
 
-    // We invoke the `inv_get()` function using our `CrossContractCall` client,
-    // supplying the `DataStore` contract address and our test user as
-    // arguments. We expect it to return the same data we already stored in the
-    // `DataStore` contract.
+    // We generate a test user, and invoke the `put` function to store some data
+    // in the DataStore contract.
+    let u1 = Address::random(&env);
+    storage_contract_client.put(&u1, &bytes![&env, 0x48656c6c6f20536f726f62616e21]); // This is the hex value for "Hello Soroban!"
+
+    // We invoke the `inv_get` function using our CrossContractCall client,
+    // supplying the DataStore contract address and our test user as arguments.
+    // We expect it to return the same data we already stored in the DataStore
+    // contract.
     assert_eq!(
         cross_call_contract_client.inv_get(&storage_contract_address, &u1),
         bytes![&env, 0x48656c6c6f20536f726f62616e21]
