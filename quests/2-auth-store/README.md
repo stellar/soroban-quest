ee5fa1469d674f4255fa6b0fa937a6f42250d2fe
Elliot Voris <elliot@stellar.org>
2024-01-03 15:34:46 -0600

    Quest 2 updates (#17)
    * updates to quest 2 README
    
    * update source code for quest 2 contract

--- quests/2-auth-store/README.md
+++ quests/2-auth-store/README.md
@@ -7,9 +7,9 @@ theory and get away with rushing into this quest? Do you feel comfortable
 completely mucking up your account before you even understand the assignment?
 
 **In this quest, you will deploy the quest 2 Auth Store contract to the Stellar
-Testnet. Then you must successfully invoke the `put()` function to store some
-data on the ledger, and then successfully invoke the `get()` function to
-retrieve that same data.**
+Testnet. Then you must successfully invoke the `put` function to store some data
+on the ledger, and then successfully invoke the `get` function to retrieve that
+same data.**
 
 ## Table of Contents <!-- omit in toc -->
 
@@ -17,13 +17,13 @@ retrieve that same data.**
 - [How to Play](#how-to-play)
 - [The Task at Hand](#the-task-at-hand)
   - [Explore the Contract Code](#explore-the-contract-code)
-  - [Expiration and Storage Types](#expiration-and-storage-types)
+  - [State Archival and Storage Types](#state-archival-and-storage-types)
     - [Temporary Storage](#temporary-storage)
     - [Persistent Storage](#persistent-storage)
     - [Instance Storage](#instance-storage)
   - [Storing Data](#storing-data)
   - [Retrieving Data](#retrieving-data)
-  - [Bumping Lifetimes](#bumping-lifetimes)
+  - [Extending TTLs](#extending-ttls)
   - [Simple Authentication](#simple-authentication)
 - [Further Reading](#further-reading)
 - [Still Stuck?](#still-stuck)
@@ -52,28 +52,23 @@ Just like all the quests, this quest's [`lib.rs`](src/lib.rs) and
 explanations of what is happening. Be sure to check them out and read through
 what the contract is up to.
 
-**Important**: This quest has very complete comments and documentation
+**Important**: This quest has pretty complete comments and documentation
 (particularly in the tests) in those two files. It will go **miles** to help
 your understanding of Soroban, if you read through those files, and take the
 time to understand what's happening.
 
-### Expiration and Storage Types
-
-> **Note**: Preview 11 saw continued development and some breaking changes due
-> to the implementations of state caching and a tiered set of storage types.
-> We've updated this quest to include those changes. Although changes may occur
-> in future releases, this document is current at the time of writing.
+### State Archival and Storage Types
 
 Do you know what is _incredibly unique_ and _amazingly powerful_ about Soroban?
-The answer might surprise you: [**State Caching**][state-caching]. A real-world
-problem within the blockchain space is that of "ledger bloat" (i.e., the
-indefinite storage of enormous amounts of data that are supposed to live on the
-blockchain "forever.") Simply put, there is no reasonable way for users or
+The answer might surprise you: [**State Archival**][state-archival]. A
+real-world problem within the blockchain space is that of "ledger bloat" (i.e.,
+the indefinite storage of enormous amounts of data that are supposed to live on
+the blockchain "forever.") Simply put, there is no reasonable way for users or
 developers to make a **one-time** payment, and have their data live on the
 blockchain until the sun burns out. Whether you think about it from an economic
 standpoint, through a sustainability lens, or just are concerned about making
 the network operate in a healthy manner: there _has_ to be some mechanism to
-"deactivate" stale entries on the blockchain.
+"archive" stale entries on the blockchain.
 
 Soroban is tackling this problem head-on with it's novel system of "rent" for
 ledger entries. Each ledger entry gets to live on the blockchain as long as it
@@ -81,7 +76,8 @@ has paid a sufficient rent balance. Even if it does run out of rent, the
 persistent data entry can be restored! This means the ledger data acts _as if_
 it is stored in the ledger forever, without requiring nearly as much long-term
 overhead to keep the network operating. Of course, if you don't want your ledger
-entries to expire, you can "bump" them at any time to keep the rent paid up.
+entries to be archived, you can extend the time to live (TTL) of that entry at
+any time to keep the rent paid up.
 
 Soroban has three types of Storage: `Temporary`, `Persistent`, and `Instance`.
 
@@ -93,8 +89,8 @@ Soroban has three types of Storage: `Temporary`, `Persistent`, and `Instance`.
   price oracles, signatures, etc.)
 - The default "lifetime" for temporary storage is **16 ledgers** (with an
   estimated 5 seconds per ledger, that lifetime is roughly 80 seconds).
-- The entry will become inactive once the lifetime ends (unless a `bump` has
-  been invoked on it). Once inactive, the the ledger entry is **permanently**
+- The entry will become "dead" once the lifetime ends (unless the entry's TTL
+  has been extended). Once dead, the the ledger entry is **permanently**
   deleted.
 - Later on, the entry _can_ be re-created, but it _cannot_ be restored.
 
@@ -106,10 +102,10 @@ Soroban has three types of Storage: `Temporary`, `Persistent`, and `Instance`.
   suitable to store temporarily. (e.g., user balances)
 - The default "lifetime" for persistent storage is **4,096 ledgers** (with an
   estimated 5 seconds per ledger, that lifetime is roughly 6 hours)
-- The entry will become inactive once the lifetime ends (unless a `bump` has
-  been invoked on it). Once inactive (which can happen while the contract
-  instance is still active), the ledger is moved to the "Inactive State Store"
-  (ISS) and the entry becomes inaccessible to the network.
+- The entry will be archived once the lifetime ends (unless the entry's TTL has
+  been extended). Once archived (which can happen while the contract instance is
+  still active), the ledger is moved to the "Inactive State Store" (ISS) and the
+  entry becomes inaccessible to the network.
 - Later on, the entry _cannot_ be re-created, but it _can_ be restored (this
   prevents over-writing a restorable ledger entry with different data).
 
@@ -122,11 +118,10 @@ Soroban has three types of Storage: `Temporary`, `Persistent`, and `Instance`.
 - Shares the "lifetime" of the contract instance. If the contract instance is
   still active, the instance storage data is guaranteed to be active. (Contract
   instances use the same default "lifetime" as `Persistent`).
-- The entry will become inactive only if the contract instance becomes inactive
-  (unless a `bump` has been invoked on the contract instance itself.) Once the
-  contract instance is inactive, the instance storage inactivates alongside it,
-  and both are moved to the "Inactive State Store" (ISS) and become inaccessible
-  to the network.
+- The entry will be archived only if the contract instance becomes inactive
+  (unless the entry's TTL has been extended). Once the contract instance is
+  inactive, the instance storage inactivates alongside it, and both are moved to
+  the "Inactive State Store" (ISS) and become inaccessible to the network.
 - Later on, the contract instance _cannot_ be re-created, but it _can_ be
   restored (this prevents over-writing a restorable contract with different
   data). If a contract is restored, its instance storage is restored as well.
@@ -153,7 +148,7 @@ us a simple method of authenticating a user. Only someone who could successfully
 sign for an address is permitted to store data in this contract associated with
 that address.
 
-_Invoke the contract's `put()` function to store some data into the contract's
+_Invoke the contract's `put` function to store some data into the contract's
 ledger entry for your quest account._
 
 ### Retrieving Data
@@ -164,49 +159,50 @@ of `temporary`, `persistent`, or `instance`) is available to us. When called
 with a `key` that corresponds to data the contract has previously stored, we get
 the `value` stored alongside it in return.
 
-The contract's `get()` function will retrieve stored data associated with any
+The contract's `get` function will retrieve stored data associated with any
 address. When supplied with an `Address` as an argument, this function will
 search for stored data corresponding to that address.
 
-_Invoke the contract's `get()` function to retrieve contract data associated
-with your quest account._
+_Invoke the contract's `get` function to retrieve contract data associated with
+your quest account._
 
-### Bumping Lifetimes
+### Extending TTLs
 
-For every storage entry, there is the potential to `bump()` the entry's lifetime
-to expire at some time further in the future. The Stellar network has lifetime
-values for storage types that are configurable (by means of a validator vote).
-The current set of values for these lifetime settings are listed in the
-following table. The values are listed in "number of ledgers" and a typical
-ledger will settle in an estimated 5 seconds.
+For every storage entry, there is the potential to extend the entry's lifetime
+until some time further in the future. The Stellar network has lifetime values
+for storage types that are configurable (by means of a validator vote). The
+current set of values for these lifetime settings are listed in the following
+table. The values are listed in "number of ledgers" and a typical ledger will
+settle in an estimated 5 seconds.
 
 | Storage Type | Minimum Lifetime | Maximum Lifetime    | Default Lifetime |
 | ------------ | ---------------- | ------------------- | ---------------- |
 | Temporary    | 16 (~80 seconds) | 535,680 (~31 days)  | 16 (~80 seconds) |
-| Persistent   | 10 (~50 seconds) | 535,680 (~31 days)  | 4,096 (~6 hours) |
-| Instance     | 10 (~50 seconds) | 535,680 (~31 days)  | 4,096 (~6 hours) |
+| Persistent   | 4,096 (~6 hours) | 535,680 (~31 days)  | 4,096 (~6 hours) |
+| Instance     | 4,096 (~6 hours) | 535,680 (~31 days)  | 4,096 (~6 hours) |
 
 Because the `temporary` storage type has such a short default lifetime, our
-contract code immediately `bump()`s the storage entry to the maximum lifetime
-when it is created. This is one technique to bump ledger entries, but certainly
-not the only method. You can also utilize a [`BumpFootprintExpirationOp`][bump]
-operation inside a Stellar transaction to accomplish the same thing. The
-`soroban` CLI also has a command to help facilitate these bump transactions:
+contract code immediately extends the TTL of the entry to the maximum lifetime
+when it is created. This is one technique to extend ledger entry lifetimes, but
+certainly not the only method. You can also utilize a
+[`ExtendFootprintTTLOp`][extend-ttl] operation inside a Stellar transaction to
+accomplish the same thing. The `soroban` CLI also has a command to help
+facilitate these TTL extension transactions.
 
 ```bash
-soroban contract bump \
+soroban contract extend \
     --id  CARCWZOD26AJQ42VRJ3UYC3MJNGJV5UHO4VFHV5FWLVIKDCJ4CZOJXII \
     --key KeySymbol \
     --durability temporary \
-    --ledgers-to-expire 100
+    --ledgers-to-extend 100
 ```
 
 When you specify a new lifetime when bumping a ledger entry, it ensures that the
 expiration is _at least_ that many ledgers in the future (from the time of
-invocation). For example, if a ledger entry is bumped by 100 ledgers and the
+invocation). For example, if a ledger entry is extended by 100 ledgers and the
 current lifetime is 50, the lifetime will be extended to 100. If a ledger entry
-is bumped by 100 ledgers and the current lifetime is 150, the lifetime will not
-be extended.
+is extended by 100 ledgers and the current lifetime is 150, the lifetime will
+not be extended.
 
 ### Simple Authentication
 
@@ -226,8 +222,8 @@ authentication method that can be made both quite simple and quite complex.
   discussion about this method of storing and retrieving data.
 - A discussion of more advanced authentication methods can be found in the
   [auth][auth-example] how-to guide.
-- You can learn more about [persisting data][persist-data] in the "Learn"
-  section of the Soroban documentation.
+- You can learn more about [persisting data][persist-data] in the Soroban
+  documentation.
 
 ## Still Stuck?
 
@@ -239,5 +235,5 @@ got a couple of suggestions for where you might go from here.
 [data-example]: https://soroban.stellar.org/docs/getting-started/storing-data
 [auth-example]: https://soroban.stellar.org/docs/basic-tutorials/auth
 [persist-data]: https://soroban.stellar.org/docs/fundamentals-and-concepts/persisting-data
-[state-caching]: https://soroban.stellar.org/docs/fundamentals-and-concepts/state-expiration
-[bump]: https://soroban.stellar.org/docs/fundamentals-and-concepts/state-expiration#bumpfootprintexpirationop
+[state-archival]: https://soroban.stellar.org/docs/fundamentals-and-concepts/state-archival
+[extend-ttl]: https://soroban.stellar.org/docs/fundamentals-and-concepts/state-archival#extendfootprintttlop
--- quests/2-auth-store/src/test.rs
+++ quests/2-auth-store/src/test.rs
@@ -1,19 +1,23 @@
 #![cfg(test)]
+extern crate std;
 
 use super::*;
 
-use soroban_sdk::{testutils::Address as _, Address, Env};
+use soroban_sdk::{
+    testutils::{Address as _, AuthorizedFunction, AuthorizedInvocation},
+    Address, Bytes, Env, IntoVal, Symbol,
+};
 
 /// These tests are a a lot more interesting and much more involved than the
 /// first quest, so let's dive into them a bit deeper. We are testing a few
 /// different scenarios with invocations as both a "user" address and as a
 /// "contract" address.
 
-/// The first function, `test_store()`, will test the values that are being
-/// stored by our contract. This is accomplished by generating a couple user
-/// addresses, storing data as those users, and ensuring retrieved data matches
-/// what we would expect. We are also checking against a keypair that hasn't
-/// stored any data, ensuring we receive Bytes of length 0 in return.
+/// The first function, `test_store`, will test the values that are being stored
+/// by our contract. This is accomplished by generating a couple user addresses,
+/// storing data as those users, and ensuring retrieved data matches what we
+/// would expect. We are also checking against a keypair that hasn't stored any
+/// data, ensuring we receive Bytes of length 0 in return.
 #[test]
 fn test_store() {
     // Here we register the DataStore contract in a default Soroban environment,
@@ -24,51 +28,103 @@ fn test_store() {
 
     // The `mock_all_auths()` function will treat any further contract
     // invocations as if they had succeeded. This feature is only available as a
-    // testing tool, but makes it easier to write tests when you're not as
-    // concerned about valid authorization.
+    // testing tool, but makes it easier to write tests so you can be sure the
+    // authorization of a contract is working properly.
     env.mock_all_auths();
 
     // We're generating two test addresses, `u1` and `u2` that will be the
     // invokers of the contract functions.
-    let u1 = Address::random(&env);
-    let u2 = Address::random(&env);
+    let u1 = Address::generate(&env);
+    let u2 = Address::generate(&env);
 
     // For our `u1` address, we store the `Bytes` representation of "Hello
-    // Soroban!" using the contract's `put()` function.
+    // Soroban!" using the contract's `put` function.
     client.put(&u1, &bytes!(&env, 0x48656c6c6f20536f726f62616e21)); // This is the hex value for "Hello Soroban!"
 
-    // We then use the contract's `get()` function to ensure we receive back the
+    // We take this opportunity to make sure the invocation of the `put`
+    // function was authenticated (mocked, at least) the way we expect. We are
+    // asserting that `env.auths()` (where the authentication is "recorded") is
+    // equal to a vector of the user address, and an authorized invocation of
+    // the function with its name and all its arguments. Notice we assert this
+    // immediately after the `put` function is invoked and before anything else
+    // is done with the `client`.
+    assert_eq!(
+        env.auths(),
+        std::vec![(
+            u1.clone(),
+            AuthorizedInvocation {
+                function: AuthorizedFunction::Contract((
+                    contract_address.clone(),
+                    Symbol::new(&env, "put"),
+                    (
+                        u1.clone(),
+                        Bytes::from_slice(
+                            &env,
+                            &[72, 101, 108, 108, 111, 32, 83, 111, 114, 111, 98, 97, 110, 33] // This is the u8 Bytes array for "Hello Soroban!"
+                        )
+                    )
+                        .into_val(&env),
+                )),
+                sub_invocations: std::vec![]
+            }
+        )]
+    );
+
+    // We then use the contract's `get` function to ensure we receive back the
     // expected value.
     assert_eq!(
         client.get(&u1),
         bytes!(&env, 0x48656c6c6f20536f726f62616e21)
     );
 
-    // Before storing any value as the `u2` address, we check to ensure `get()`
+    // Before storing any value as the `u2` address, we check to ensure `get`
     // returns 0 Bytes (i.e. the address has no data to get).
     assert_eq!(client.get(&u2).len(), 0);
 
-    // Now, as `u2`, we invoke the `put()` function, storing the `Bytes`
+    // Now, as `u2`, we invoke the `put` function, storing the `Bytes`
     // representation of "Soroban Quest 2".
     client.put(&u2, &bytes![&env, 0x536f726f62616e2051756573742032]); // This is the hex value for "Soroban Quest 2"
 
-    // We now assert that `get()` should return the same value back to us.
+    // Again, we assert that the mock authorization is what we expect.
+    assert_eq!(
+        env.auths(),
+        std::vec![(
+            u2.clone(),
+            AuthorizedInvocation {
+                function: AuthorizedFunction::Contract((
+                    contract_address.clone(),
+                    Symbol::new(&env, "put"),
+                    (
+                        u2.clone(),
+                        Bytes::from_slice(
+                            &env,
+                            &[83, 111, 114, 111, 98, 97, 110, 32, 81, 117, 101, 115, 116, 32, 50] // This is the u8 Bytes array for "Soroban Quest 2"
+                        )
+                    )
+                        .into_val(&env),
+                )),
+                sub_invocations: std::vec![]
+            }
+        )]
+    );
+
+    // We now assert that `get` should return the same value back to us.
     assert_eq!(
         client.get(&u2),
         bytes![&env, 0x536f726f62616e2051756573742032]
     );
 
     // Of course, we expect that the data for `u1` has not been overwritten by
-    // `u2` invoking the `put()` function.
+    // `u2` invoking the `put` function.
     assert_eq!(
         client.get(&u1),
         bytes![&env, 0x48656c6c6f20536f726f62616e21]
     );
 }
 
-/// The `test_store_value_too_short()` function will attempt to store a value
-/// that is shorter than the required 11 bytes long. We expect that this will
-/// end in a panic, with the relevant error code from `error.rs`.
+/// The `test_store_value_too_short` function will attempt to store a value that
+/// is shorter than the required 11 bytes long. We expect that this will end in
+/// a panic, with the relevant error code from `error.rs`.
 #[test]
 #[should_panic(expected = "Error(Contract, #2)")]
 fn test_store_value_too_short() {
@@ -78,46 +134,57 @@ fn test_store_value_too_short() {
     let contract_address = env.register_contract(None, DataStoreContract);
     let client = DataStoreContractClient::new(&env, &contract_address);
 
-    // Disable checks for authentication. See note in first test for more info.
+    // Mock authentication checks. See note in first test for more info.
     env.mock_all_auths();
 
     // We're generating a single test address, `u1`, which will be the invoker
-    // of the contract's `put()` function.
-    let u1 = Address::random(&env);
+    // of the contract's `put` function.
+    let u1 = Address::generate(&env);
 
     // For our `u1` address, we attempt to store `Bytes(0, 7)` using the
-    // contract's `put()` function. We stop there, since we're just expecting
-    // the contract to panic with the argument that's too short.
+    // contract's `put` function. We stop there, since we're just expecting the
+    // contract to panic because the argument is too short.
     client.put(&u1, &bytes![&env, 0x007]);
 }
 
 /// For the next few tests, we are going to test our DataStore contract's
 /// behavior when it is invoked from another contract. So, we are creating a
-/// very simple smart contract here, that we can use in for that purpose. It's
+/// very simple smart contract here, that we can use for that purpose. It's
 /// quite simple, and only exists as a client to invoke the main contract's
-/// `put()` and `get()` functions.
+/// `put` and `get` functions.
 #[contract]
 pub struct CallerContract;
 
 #[contractimpl]
 impl CallerContract {
-    // This function passes our supplied `data` argument to the DataStore
-    // contract's `put()` function, associating it with this contract's Address.
+    /// This function passes our supplied `data` argument to the DataStore
+    /// contract's `put` function, associating it with this contract's Address.
+    ///
+    /// # Arguments
+    ///
+    /// * `contract_address` - address of a DataStoreContract to invoke.
+    /// * `user` - address of a user to store as the data key.
+    /// * `data` - data to store alongside the user address.
     pub fn try_put(env: Env, contract_address: Address, user: Address, data: Bytes) {
-        let cli = DataStoreContractClient::new(&env, &contract_address);
-        cli.put(&user, &data);
+        let client = DataStoreContractClient::new(&env, &contract_address);
+        client.put(&user, &data);
     }
 
-    // This function invokes the `get()` function from the DataStore contract,
-    // passing along an `owner` argument containing an Address.
+    /// This function invokes the `get` function from the DataStore contract,
+    /// passing along an `owner` argument containing an Address.
+    ///
+    /// # Arguments
+    ///
+    /// * `contract_address` - address of a DataStoreContract to invoke.
+    /// * `owner` - address of a user to retrieve corresponding data for.
     pub fn try_get(env: Env, contract_address: Address, owner: Address) -> Bytes {
-        let cli = DataStoreContractClient::new(&env, &contract_address);
-        cli.get(&owner)
+        let client = DataStoreContractClient::new(&env, &contract_address);
+        client.get(&owner)
     }
 }
 
-/// The `test_contract_store()` function will test whether a contract can invoke
-/// the `put()` function in our DataStore contract to store some data, and make
+/// The `test_contract_store` function will test whether a contract can invoke
+/// the `put` function in our DataStore contract to store some data, and make
 /// sure it's correctly storing the data by reading from the DataStore contract.
 #[test]
 fn test_contract_store() {
@@ -128,7 +195,7 @@ fn test_contract_store() {
     let data_store_contract_address = env.register_contract(None, DataStoreContract);
     let data_store_client = DataStoreContractClient::new(&env, &data_store_contract_address);
 
-    // Disable checks for authentication. See note in first test for more info.
+    // Mock authentication checks. See note in first test for more info.
     env.mock_all_auths();
 
     // We take an extra step to register our Caller contract in the Soroban
@@ -138,8 +205,8 @@ fn test_contract_store() {
     let caller_contract_address = env.register_contract(None, CallerContract);
     let caller_client = CallerContractClient::new(&env, &caller_contract_address);
 
-    // We are invoking the the DataStore contract's `put()` function using our
-    // Caller contract's `try_put()` function.
+    // We are invoking the the DataStore contract's `put` function using our
+    // Caller contract's `try_put` function.
     caller_client.try_put(
         // The address of the DataStore contract where we are storing our data
         &data_store_contract_address,
@@ -158,8 +225,8 @@ fn test_contract_store() {
     );
 }
 
-/// This `test_contract_get()` function will attempt to invoke the `get()`
-/// function of the DataStore contract, as another smart contract.
+/// This `test_contract_get` function will attempt to invoke the `get` function
+/// of the DataStore contract, as another smart contract.
 #[test]
 fn test_contract_get() {
     // Similar to all Soroban tests, we create an environment, and register the
@@ -169,7 +236,7 @@ fn test_contract_get() {
     let data_store_contract_address = env.register_contract(None, DataStoreContract);
     let data_store_client = DataStoreContractClient::new(&env, &data_store_contract_address);
 
-    // Disable checks for authentication. See note in first test for more info.
+    // Mock authentication checks. See note in first test for more info.
     env.mock_all_auths();
 
     // We take an extra step to register our Caller contract in the Soroban
@@ -178,13 +245,13 @@ fn test_contract_get() {
     let caller_contract_address = env.register_contract(None, CallerContract);
     let caller_client = CallerContractClient::new(&env, &caller_contract_address);
 
-    // We create an Address, `u1`, so we can invoke the `put()` function, and
+    // We create an Address, `u1`, so we can invoke the `put` function, and
     // test against the value we store, when called from our contract later.
-    let u1 = Address::random(&env);
+    let u1 = Address::generate(&env);
     data_store_client.put(&u1, &bytes!(&env, 0x48656c6c6f20536f726f62616e21)); // This is the hex value for "Hello Soroban!"
 
-    // We are invoking the the DataStore contract's `get()` function by using
-    // the `try_get()` function in the Caller contract. We expect our returned
+    // We are invoking the the DataStore contract's `get` function by using
+    // the `try_get` function in the Caller contract. We expect our returned
     // value to match the value we `put` before.
     let value = caller_client.try_get(&data_store_contract_address, &u1);
     assert_eq!(value, bytes!(&env, 0x48656c6c6f20536f726f62616e21));
