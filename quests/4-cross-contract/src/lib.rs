5a6d694af23b722c7d6482b760e3daed630b2390
Elliot Voris <elliot@stellar.org>
2024-01-05 12:42:19 -0600

    source code updates for quest 4

--- quests/4-cross-contract/src/lib.rs
+++ quests/4-cross-contract/src/lib.rs
@@ -1,29 +1,37 @@
+//! # Cross Contract
+//!
+//! A Soroban smart contract that calls yet another Soroban smart contract.
+
 #![no_std]
 use soroban_sdk::{contract, contractimpl, Address, Bytes, Env};
 
-/// We import the compiled binary from our DataStore contract in Quest 2,
-/// allowing us to use that contract's types and client in this contract.
+// We import the compiled binary from our DataStore contract in Quest 2,
+// allowing us to use that contract's types and client in this contract.
 mod storage_contract {
     // We do this inside a `mod{}` block to avoid collisions between type names
-    soroban_sdk::contractimport!(file = "./soroban_auth_store_contract.wasm"); // TODO: `message: not yet implemented`?!
+    soroban_sdk::contractimport!(file = "./soroban_auth_store_contract.wasm");
 }
 
 /// We define a `trait` which can be used to create shared behavior between
 /// types, and can be used to group together signatures for related functions.
 pub trait StorageCallTrait {
-    // We define in this trait that the `inv_get` function will accept:
-    // - `store_address`: the DataStore contract we are going to invoke
-    // - `owner`: the `Address` that was used to `put` data to that contract
-    // This function will return the data that was stored, in `Bytes`
+    /// We define in this trait what the `inv_get` function signature must look
+    /// like. This function will return the data that was stored, in `Bytes`
+    ///
+    /// # Arguments
+    ///
+    /// * `store_address` - the DataStore contract we are going to invoke
+    /// * `owner` - the `Address` that was used to `put` data to that contract
     fn inv_get(env: Env, store_address: Address, owner: Address) -> Bytes;
 }
 
 #[contract]
 pub struct CrossContractCallContract;
 
-/// The CrossContractCall contract implements the trait we defined earlier, and
-/// fleshes out what the `inv_get` function should do with it arguments and how
-/// it should create the value to return.
+/// Our implementation of the `CrossContractCallContract` smart contract.
+/// (enough "contracts" for you?!). It implements the trait we defined earlier,
+/// and fleshes out what the `inv_get` function should do with its arguments and
+/// how it should create the value to return.
 #[contractimpl]
 impl StorageCallTrait for CrossContractCallContract {
     /// The `inv_get` function will create a new client to the DataStore
