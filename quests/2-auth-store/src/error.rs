63cc56c5f3f1b39401e50afd48c1d5f59ecf6329
Elliot Voris <elliot@stellar.org>
2024-02-12 13:05:01 -0600

    updates for quest 2

--- quests/2-auth-store/src/error.rs
+++ quests/2-auth-store/src/error.rs
@@ -4,9 +4,6 @@ use soroban_sdk::contracterror;
 #[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
 #[repr(u32)]
 pub enum ContractError {
-    // You need to be a user to invoke this function, Cross Contract calls are
-    // only allowed to call the `get` function
-    CrossContractCallProhibited = 1,
     // Input Value too short, provide at least 11 characters (bytes)
-    InputValueTooShort = 2,
+    InputValueTooShort = 1,
 }
