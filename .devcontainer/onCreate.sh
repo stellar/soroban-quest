1d321c7b20f606042c99f537c5a2574e585a5dfc
chris.anatalio <chris.anatalio@stellar.org>
2025-03-21 15:15:55 -0700

    Add node

--- .devcontainer/onCreate.sh
+++ .devcontainer/onCreate.sh
@@ -6,11 +6,10 @@ echo "source <(stellar completion --shell bash)" >> ~/.bashrc
 echo "Enabled Stellar CLI auto-completion"
 
 # Install warp terminal
-sudo apt-get install wget gpg
 wget -qO- https://releases.warp.dev/linux/keys/warp.asc | gpg --dearmor > warpdotdev.gpg
 sudo install -D -o root -g root -m 644 warpdotdev.gpg /etc/apt/keyrings/warpdotdev.gpg
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/warpdotdev.gpg] https://releases.warp.dev/linux/deb stable main" > /etc/apt/sources.list.d/warpdotdev.list'
 rm warpdotdev.gpg
-sudo apt update && sudo apt install warp-terminal
+sudo apt update && sudo echo "Y" | apt install warp-terminal
 
 chmod +x client/test.ts
