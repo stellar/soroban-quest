3afecde149b2196b340006e308ee3a227cd9feac
chris.anatalio <chris.anatalio@stellar.org>
2025-03-22 01:22:54 -0700

    Add node

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -2,9 +2,20 @@
 set -e
 
 # Populate dotfiles
-mkdir -p .vscode/ || { echo "Command failed"; exit 1; }
-mv -n .devcontainer/dotfiles/settings.json .vscode/ || { echo "Command failed"; exit 1; }
-mv -n .devcontainer/dotfiles/tasks.json .vscode/ || { echo "Command failed"; exit 1; }
+mkdir -p .vscode/
 
-mv -n .devcontainer/dotfiles/.bashrc $HOME/ || { echo "Command failed"; exit 1; }
-mv -n .devcontainer/dotfiles/.zshrc $HOME/ || { echo "Command failed"; exit 1; }
+if [ -e .devcontainer/dotfiles/settings.json ]; then
+  mv -n .devcontainer/dotfiles/settings.json .vscode/
+fi
+
+if [ -e .devcontainer/dotfiles/tasks.json ]; then
+  mv -n .devcontainer/dotfiles/tasks.json .vscode/
+fi
+
+if [ -e .devcontainer/dotfiles/.bashrc ]; then
+  mv -n .devcontainer/dotfiles/.bashrc $HOME/
+fi
+
+if [ -e .devcontainer/dotfiles/.zshrc ]; then
+  mv -n .devcontainer/dotfiles/.zshrc $HOME/
+fi
