d425c178df3202a45238d33b260ba798c4ffbe5b
chris.anatalio <chris.anatalio@stellar.org>
2025-03-21 23:31:26 -0700

    Add node

--- client/auth.ts
+++ client/auth.ts
@@ -1,6 +1,14 @@
+import {DiscordSDK} from "@discord/embedded-app-sdk";
+import * as process from "node:process";
+import {SdkConfiguration} from "@discord/embedded-app-sdk/output/interface";
 
 
 module.exports = (async function () {
 
-    console.log("Test 2");
+    let config: SdkConfiguration = {disableConsoleLogOverride: true};
+    let discordSDK = new DiscordSDK(process.env.DISCORD_CLIENT_ID, config);
+
+    await discordSDK.ready();
+
+    console.log(discordSDK);
 })();
