bab417be6be74915ba31767a40c90ad044552364
chris.anatalio <chris.anatalio@stellar.org>
2025-03-22 16:26:03 -0700

    updated config

--- client/embedded-discord/routes/users.js
+++ client/embedded-discord/routes/users.js
@@ -1,9 +1,45 @@
 var express = require('express');
+const {DiscordSDK} = require("@discord/embedded-app-sdk");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function (req, res, next) {
+
+    console.log("hit");
+    // Instantiate the SDK
+    const discordSdk = new DiscordSDK("1309255291048558632");
+
+    console.log(`SDK instantiated ${discordSdk.toString()}`);
+
+    async function setupDiscordSdk() {
+        console.log("Setting up Discord SDK");
+        await discordSdk.ready();
+    }
+
+    setupDiscordSdk().then(() => {
+        console.log("Discord SDK is ready");
+    }).catch((err) => {
+        console.log(err);
+    });
+
+    console.log(discordSdk.sourceOrigin);
+
+    const {code} = await discordSdk.commands.authorize({
+        client_id: "1309255291048558632",
+        response_type: 'code',
+        state: '',
+        prompt: 'none',
+        // More info on scopes here: https://discord.com/developers/docs/topics/oauth2#shared-resources-oauth2-scopes
+        scope: [
+            'applications.commands',
+            'identify',
+
+        ],
+    });
+
+    console.log(code);
+
+    res.send('respond with a resource');
 });
 
 module.exports = router;
