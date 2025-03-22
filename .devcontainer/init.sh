e34f824493663fd160a7e53fc25fdbd2c0680b72
chris.anatalio <chris.anatalio@stellar.org>
2025-03-21 19:09:40 -0700

    Add node

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -6,7 +6,10 @@ RUN apt-get update && bash /tmp/library-scripts/common-debian.sh
 
 # Populate dotfiles
 mkdir ./.vscode
-mv .devcontainer/dotfiles/settings.json ../.vscode/
-mv .devcontainer/dotfiles/tasks.json ../.vscode/
+mv .devcontainer/dotfiles/settings.json .vscode/
+mv .devcontainer/dotfiles/tasks.json .vscode/
 
 mv .devcontainer/dotfiles/.bashrc "$HOME"
+
+# zsh prompt
+git config devcontainers-theme.hide-status 1
