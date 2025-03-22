7cfe8263cb57eb4427df7df7fa93b029215ed010
chris.anatalio <chris.anatalio@stellar.org>
2025-03-21 18:34:25 -0700

    Add node

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+# Common scripts
 COPY library-scripts/common-debian.sh /tmp/library-scripts/
 RUN apt-get update && bash /tmp/library-scripts/common-debian.sh
 
-
+# Populate dotfiles
 mkdir ./.vscode
-mv mv dotfiles/settings.json ./.vscode/settings.json
-mv mv dotfiles/tasks.json ./.vscode/tasks.json
+mv mv dotfiles/settings.json ./.vscode/
+mv mv dotfiles/tasks.json ./.vscode/
